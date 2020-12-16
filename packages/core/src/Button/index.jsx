c61c6dc9507d00c8db87d6b305e0fd52b3aca909
Douglas Zaltron <douglas.zaltron@luizalabs.com>
2020-12-16 15:09:59 -0300

    feat(button): add material-ui/core

--- packages/core/src/Button/index.jsx
+++ packages/core/src/Button/index.jsx
@@ -1,1 +1,1 @@
-export * from '@material-ui/core/Button';
+export { default } from '@material-ui/core/Button';
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,3 @@
 /* eslint-disable import/export */
+export { default as Button } from './Button';
 export * from './Button';
