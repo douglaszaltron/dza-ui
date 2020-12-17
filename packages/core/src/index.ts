c991b57970909247d3b4d0ba295227724d3b93fd
Douglas Zaltron <douglas.zaltron@luizalabs.com>
2020-12-17 00:38:11 -0300

    feat(button-badge-chip): add variants

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,8 @@
 /* eslint-disable import/export */
+export { default as Badge } from './Badge';
+export { default as Button } from './Button';
+export { default as Chip } from './Chip';
+
 export * from './Badge';
 export * from './Button';
 export * from './Chip';
